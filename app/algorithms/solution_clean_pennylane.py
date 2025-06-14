f1758491fe8a445370f0194b5a85d4b021dbe1f7
Dannyvr <dannyalfvr97@gmail.com>
2025-06-13 23:54:00 -0600

    pennylane optimization

--- app/algorithms/solution_clean_pennylane.py
+++ app/algorithms/solution_clean_pennylane.py
@@ -1,10 +1,11 @@
-
 import numpy as np
 import pennylane as qml
 from pennylane import numpy as pnp
 from PIL import Image
 from matplotlib import pyplot as plt
 from scipy.linalg import norm
+from itertools import product
+
 
 class VQSVD():
     def __init__(self, matrix: pnp.ndarray, 
@@ -12,7 +13,6 @@ class VQSVD():
                  num_qubits: int, depth: int, rank: int, 
                  lr: float, itr: int, seed: int):
 
-        # Hyperparameters
         self.rank = rank
         self.lr = lr
         self.itr = itr
@@ -21,45 +21,44 @@ class VQSVD():
 
         pnp.random.seed(seed)
 
-        # Parameter shapes for U and V
-        self.param_shape = (depth, 2,num_qubits)
-        self.theta = pnp.random.uniform(0, 2*pnp.pi, self.param_shape, requires_grad=True)
-        self.phi = pnp.random.uniform(0, 2*pnp.pi, self.param_shape, requires_grad=True)
+        # Parametros iniciales para U y V
+        self.param_shape = (depth, 2, num_qubits)
+        self.theta = pnp.random.uniform(0, 2 * pnp.pi, self.param_shape, requires_grad=True)
+        self.phi = pnp.random.uniform(0, 2 * pnp.pi, self.param_shape, requires_grad=True)
 
-        # Ansatz function (should be defined outside the class)
-        self.ansatz = self._ansatz
         self.M = pnp.array(matrix)
         self.weight = pnp.array(weights)
-        self.U = self.get_unitary_matrix(self.ansatz, self.theta, self.num_qubits)
-        self.V = self.get_unitary_matrix(self.ansatz, self.phi, self.num_qubits)
 
+        self.dev = qml.device("lightning.qubit", wires=num_qubits)
 
-    def _ansatz(self, params, wires):
-        depth = params.shape[0]
-        num_qubits = len(wires)
-        for d in range(depth):
-            for q in range(num_qubits):
-                qml.RY(params[d, 0, q], wires=wires[q])
-                qml.RZ(params[d, 1, q], wires=wires[q])
-            for q in range(num_qubits - 1):
-                qml.CNOT(wires=[wires[q], wires[q+1]])
+        self.basis_states = [''.join(bits) for bits in product('01', repeat=num_qubits)]
 
-
-    def get_unitary_matrix(self, ansatz_fn, params, num_qubits):
-        dev = qml.device("lightning.qubit", wires=num_qubits)
-        basis_states = [format(i, f"0{num_qubits}b") for i in range(2**num_qubits)]
-        matrix_columns = []
-        @qml.qnode(dev)
-        def basis_circuit(state_bin, params):
-            # Prepare basis state
-            for j, bit in enumerate(reversed(state_bin)):
-                if bit == "1":
-                    qml.PauliX(wires=j)
-            ansatz_fn(params, wires=range(num_qubits))
+        @qml.qnode(self.dev)
+        def _basis_circuit(state_bin, params):
+            # Aplicar PauliX a los bits en 1 para preparar el estado base
+            for j in [i for i, bit in enumerate(reversed(state_bin)) if bit == "1"]:
+                qml.PauliX(wires=j)
+            self.ansatz(params, wires=range(self.num_qubits))
             return qml.state()
+        self.basis_circuit = _basis_circuit
 
-        for b in basis_states:
-            final_state = basis_circuit(b, params)
+        # Inicializar matrices unitarias
+        self.U = self.get_unitary_matrix(self.theta)
+        self.V = self.get_unitary_matrix(self.phi)
+
+    def ansatz(self, params, wires):
+        depth = params.shape[0]
+        for d in range(depth):
+            ry_layer = params[d, 0]
+            for q, w in enumerate(wires):
+                qml.RY(ry_layer[q], wires=w)
+            for q in range(len(wires) - 1):
+                qml.CNOT(wires=[wires[q], wires[q + 1]])
+
+    def get_unitary_matrix(self, params):
+        matrix_columns = []
+        for b in self.basis_states:
+            final_state = self.basis_circuit(b, params)
             matrix_columns.append(final_state)
         return pnp.column_stack(matrix_columns)
 
@@ -67,25 +66,30 @@ class VQSVD():
         return self.U
 
     def get_matrix_V(self):
-        return self.V   
+        return self.V
 
     def loss_func(self, theta, phi):
-        self.U =  self.get_unitary_matrix(self.ansatz, theta, self.num_qubits)
-        self.V =  self.get_unitary_matrix(self.ansatz, phi, self.num_qubits)
+        U = self.get_unitary_matrix(theta)
+        V = self.get_unitary_matrix(phi)
+        U_dag = pnp.conj(U.T)
+
+        M_eff = U_dag @ self.M @ V
         loss = pnp.array(0.0)
         singular_values = []
+
         for i in range(self.rank):
-            value = pnp.real(pnp.conj(self.U.T) @ self.M @ self.V)[i, i]
-            loss -= pnp.real(self.weight)[i] * value
-            singular_values.append(value)
+            val = pnp.real(M_eff[i, i])
+            loss -= pnp.real(self.weight[i]) * val
+            singular_values.append(val)
+
         return loss, singular_values
 
-    # Train the VQSVD network
     def train(self):
         loss_list, singular_value_list = [], []
         optimizer = qml.AdamOptimizer(self.lr)
         theta = self.theta
         phi = self.phi
+
         for itr in range(self.itr):
             (theta, phi), loss = optimizer.step_and_cost(
                 lambda t, p: self.loss_func(t, p)[0], theta, phi
@@ -95,12 +99,16 @@ class VQSVD():
             singular_value_list.append(singular_values)
             if itr % 10 == 0:
                 print(f'iter: {itr}, loss: {loss_val:.4f}')
-        # Save final parameters
+
         self.theta = theta
         self.phi = phi
+
+        # Actualizar las matrices finales
+        self.U = self.get_unitary_matrix(self.theta)
+        self.V = self.get_unitary_matrix(self.phi)
+
         return loss_list, singular_value_list
     
-
     def frobenius_norm_error(self, M, M_err, U_learned, singular_value_list, V_dagger_learned, RANK):        
         err_local, err_subfull, err_SVD = [], [], []
         U, D, V_dagger = np.linalg.svd(M, full_matrices=True)
@@ -114,6 +122,7 @@ class VQSVD():
             err_subfull.append(norm(M_err - recons_mat))
             err_SVD.append(norm(M_err - lowrank_mat))
         return err_local, err_subfull, err_SVD
+
 
 
 class LossPlot:
@@ -188,21 +197,37 @@ class MatGenerator:
         M = real_part + 1j * imag_part
         return M.astype('complex128')
 
+# Open the picture prepared in advance
+img = Image.open('../figures/MNIST_32.png')
+imgmat = np.array(list(img.getdata(band=0)), float)
+imgmat.shape = (img.size[1], img.size[0])
+imgmat = np.matrix(imgmat)/255
+
+
+
+# Convert the image into numpy array
+def Mat_generator():
+    imgmat = np.array(list(img.getdata(band=0)), float)
+    imgmat.shape = (img.size[1], img.size[0])
+    lenna = np.matrix(imgmat)
+    return lenna.astype('complex128')
+
 
 # Set circuit parameters
 cir_depth = 40      # depth of circuit
 num_qubits = 5      # Number of qubits
 
 # Hyper-parameters
-RANK = 8           # Set the number of rank you want to learn
-ITR = 100           # Number of iterations
+RANK = 8            # Set the number of rank you want to learn
+ITR = 10           # Number of iterations
 LR = 0.02           # Learning rate
 SEED = 14           # Random number seed
 
 # Set the learning weight
 weight = np.arange(2 * RANK, 0, -2).astype('complex128')
 
-# Record the optimization process
+M_err = Mat_generator()
+
 loss_list, singular_value_list = [], []
 U_learned, V_dagger_learned = [], []
 matrix_gen = MatGenerator(num_qubits)
